ddc67bb88d33eb6ea543bbeebc3aa24a2afe5cfa
David-Pena <dev.davidpe@gmail.com>
2024-06-30 20:20:58 -0700

    chore: product types

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,46 +1,12 @@
-import { StyleSheet, Image } from "react-native";
-import { Text, View } from "@/src/components/Themed";
-import Colors from "@/src/constants/Colors";
+import { View } from "@/src/components/Themed";
 import products from "@/assets/data/products";
-
-const product = products[0];
-
-const ProductListItem = () => {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}</Text>
-    </View>
-  );
-};
+import ProductListItem from "@/src/components/ProductListItem";
 
 export default function TabOneScreen() {
   return (
     <View>
-      <ProductListItem />
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[5]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import { Product } from "../types";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+const ProductListItem = ({ product }: { product: Product }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
